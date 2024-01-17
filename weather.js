2e736a519dc904c4c88f0a8f34e190681e61b7d9
David Domínguez <ddominguezinf11@gmail.com>
2024-01-17 19:44:17 -0400

    Actualización de documentación de weather.js

--- weather.js
+++ weather.js
@@ -2,17 +2,17 @@ const result = document.querySelector('.result');
 const form = document.querySelector('.get-weather');
 const stringInput = document.getElementById('city_country');
 
-
+//Evento al presionar el botón "Buscar Clima"
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+    //constante que toma el valor String del input    
     const rawInput = stringInput.value;
+    //separar la ciudad y país del String tomando la "," como separador
     const splitedInput = rawInput.split(',');
     const cityName = splitedInput[0].trim();
     const countryName = splitedInput.slice(1).join(',').trim();
-    console.log(cityName);
-    console.log(countryName);
 
+    //Verificar si ambos campos están vacíos 
     if (cityName === '' & countryName === '') {
         showError('No se ha insertado ningún país o ciudad...');
         return;
@@ -22,7 +22,7 @@ form.addEventListener('submit', (e) => {
     
     //console.log(countryName);
 })
-
+//función para hacer la solicitud a la API
 function callAPI(city, country){
     const apiKey = '62440dba3fbaf7c477ad5395efd917eb';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
@@ -38,25 +38,23 @@ function callAPI(city, country){
                 clearHTML();
                 showWeather(dataJSON);
             }
-            
-            
-            //console.log(dataJSON);
+
         })
         .catch(error => {
             console.log(error);
         })
 }
-
+//función que toma los datos del Json y los muestra en el doc html
 function showWeather(data){
     const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data;
-
+    //convertir kerlvin a celcius
     const degrees = kelvinToCentigrade(temp);
     const min = kelvinToCentigrade(temp_min);
     const max = kelvinToCentigrade(temp_max);
 
     const content = document.createElement('div');
     content.innerHTML = `
-        <h5>Clima en ${name}</h5>
+        <h5>Clima en ${name}</h5>               
         <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
         <h2>${degrees}°C</h2>
         <p>Max: ${max}°C</p>
@@ -65,13 +63,8 @@ function showWeather(data){
 
     result.appendChild(content);
 
-    /* console.log(name);
-    console.log(temp);
-    console.log(temp_max);
-    console.log(temp_min);
-    console.log(arr.icon); */
 }
-
+//función para mostrar mensaje de error
 function showError(message){
     //console.log(message);
     const alert = document.createElement('p');
@@ -83,11 +76,11 @@ function showError(message){
         alert.remove();
     }, 3000);
 }
-
+//función para convertir kelvin a celcius
 function kelvinToCentigrade(temp){
     return parseInt(temp - 273.15);
 }
-
+//función para limpiar los resultados anteriores
 function clearHTML(){
     result.innerHTML = '';
 }
